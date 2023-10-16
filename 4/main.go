198f54d95c49b46e1b48646784d2b547344e30bc
Daniel Paulino <daniel-paulino@hotmail.com>
2023-10-16 01:10:15 -0300

    4 - fifth class

--- 4/main.go
+++ 4/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+)
+
+const a = "Hellow World"
+
+// ID Creating a type of variable
+type ID int
+
+var (
+	b bool = true
+	c int
+	d string
+	e float64 = 1.2
+	f ID      = 21
+)
+
+func main() {
+	fmt.Println("Testing about this.")
+	fmt.Printf("Check the type %T", e)
+	fmt.Printf("\nCheck the value %v", e)
+	fmt.Printf("\nCheck the type %T", f)
+}
