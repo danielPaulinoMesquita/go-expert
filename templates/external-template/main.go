3fb953fdca0b53d448b7d3fe1c0030e46ba5f4c9
Daniel Paulino <daniel-paulino@hotmail.com>
2023-11-08 02:38:07 -0300

    32 - External template

---
+++ templates/external-template/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	// you must have to be into the directory to run "go run main.go"
+	t := template.Must(template.New("template.html").ParseFiles("template.html"))
+	err := t.Execute(os.Stdout, Cursos{
+		{"Go", 40},
+		{"Java", 100},
+		{"Python", 50},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+}
