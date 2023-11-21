b637d23a2415a9eed8a98c368b33e19fc87848d9
Daniel Paulino <daniel-paulino@hotmail.com>
2023-11-21 00:04:31 -0300

    42- GORM - Belongs To

---
+++ database/3/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// Category
+type Category struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+}
+
+// Product gorm brings some properties or columns to manipulate the data. It is so useful
+type Product struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	CategoryID int
+	Category   Category
+	gorm.Model
+}
+
+func main() {
+	// charset set up the time default for using in the table
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Now, it's necessary to pass AutoMigrate
+	err = db.AutoMigrate(&Product{}, &Category{})
+	if err != nil {
+		return
+	}
+
+	//// TODO create Category
+	category := Category{Name: "Eletronicos"}
+	db.Create(&category)
+
+	////TODO create Product
+	//db.Create(&Product{
+	//	Name:  "VideoGame",
+	//	Price: 2000.00,
+	//})
+	//
+	////TODO create Product with category
+	product := Product{
+		Name:       "Notebook",
+		Price:      1000.00,
+		CategoryID: category.ID,
+	}
+	db.Create(&product)
+
+	var products []Product
+	db.Preload("Category").Find(&products)
+	for _, product := range products {
+		fmt.Println(product.Name, product.Category.Name)
+	}
+
+}
