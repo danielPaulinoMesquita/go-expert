8a36e9531a5f53c597cf47d80ce7581333f95ba0
Daniel Paulino <daniel-paulino@hotmail.com>
2023-12-01 00:41:09 -0300

    53- Testing with Mocks from testify mock

--- testing-go/testify/tax.go
+++ testing-go/testify/tax.go
@@ -2,7 +2,32 @@ package testify
 
 import "errors"
 
-func CalculateTax(amount float64) (float64, error) {
+type Repository interface {
+	SaveTax(amount float64) error
+}
+
+func CalculateTax(amount float64) float64 {
+	if amount <= 0 {
+		return 0.0
+	}
+
+	if amount >= 1000 && amount <= 2000 {
+		return 10.0
+	}
+
+	if amount >= 2000 {
+		return 20.0
+	}
+
+	return 5.0
+}
+
+func CalculateTaxAndSave(amount float64, repository Repository) error {
+	tax := CalculateTax(amount)
+	return repository.SaveTax(tax)
+}
+
+func CalculateTax2(amount float64) (float64, error) {
 	if amount <= 0 { // if the rule was amount < 0 it wll throw an error in Fuzzy
 		return 0.0, errors.New("amount must be greater than 0")
 	}
@@ -10,10 +35,6 @@ func CalculateTax(amount float64) (float64, error) {
 	if amount >= 1000 && amount <= 2000 {
 		return 10.0, nil
 	}
-
-	//if amount >= 1000 { // <- If this code stay with this rule, it makes error on the Fuzzy
-	//	return 10.0
-	//}
 
 	if amount >= 2000 {
 		return 20.0, nil
--- testing-go/testify/tax_test.go
+++ testing-go/testify/tax_test.go
@@ -1,18 +1,39 @@
 package testify
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestCalculateTax(t *testing.T) {
-	tax, err := CalculateTax(1000.0)
+	tax, err := CalculateTax2(1000.0)
 	assert.Nil(t, err)
 	assert.Equal(t, 10.0, tax)
 
-	tax, err = CalculateTax(0)
+	tax, err = CalculateTax2(0)
 	assert.Error(t, err, "amount must be greater than 0")
 	assert.Equal(t, 0.0, tax)
 	assert.Contains(t, err.Error(), "greater than 0")
 
+}
+
+func TestCalculateTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil).Twice() // Determining limit to call CalculateTaxAndSave (only 2 times) that should return 10
+	repository.On("SaveTax", 10.0).Return(nil)
+	repository.On("SaveTax", 0.0).Return(errors.New("error saving tax"))
+	// repository.On("SaveTax", mock.Anything).Return(errors.New("error saving tax")) <- you can use mock.Anything
+
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	err1 := CalculateTaxAndSave(1000.0, repository) // this second call is only to show the log noticing the amount of call exceeded
+	assert.Nil(t, err1)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err, "error saving tax")
+
+	repository.AssertExpectations(t)
+	repository.AssertNumberOfCalls(t, "SaveTax", 3)
 }
