722b031eba331781b8590afa837a72bb4cd26fbe
Daniel Paulino <daniel-paulino@hotmail.com>
2023-12-04 00:37:14 -0300

    60- Finishing Tests for USER DB

--- second-part-course/internal/infra/database/user_db_test.go
+++ second-part-course/internal/infra/database/user_db_test.go
@@ -31,3 +31,25 @@ func TestCreateUser(t *testing.T) {
 	assert.NotEmpty(t, userFound.Password)
 
 }
+
+func TestUserFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("John", "j@j.com", "1234567")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+
+}
