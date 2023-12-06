7c5da048feb03a54edd581d204e036f85764f1e1
Daniel Paulino <daniel-paulino@hotmail.com>
2023-12-06 01:09:42 -0300

    61- Created PRODUCT DB and some methods

--- second-part-course/internal/infra/database/interface.go
+++ second-part-course/internal/infra/database/interface.go
@@ -1,8 +1,18 @@
 package database
 
-import "github.com/devfullcycle/dan/goexpert/internal/entity"
+import (
+	"github.com/devfullcycle/dan/goexpert/internal/entity"
+)
 
 type UserInterface interface {
 	Create(user *entity.User) error
 	FindByEmail(emaild string) (*entity.User, error)
+}
+
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindById(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
 }
