4600c5c86ee111f5b3e4f3903ee36b139df2c8fb
Daniel Paulino <daniel-paulino@hotmail.com>
2023-12-24 00:24:20 -0300

    90.1- Select, second way.

--- third-party-course/multithreading/Channels/Select/main.go
+++ third-party-course/multithreading/Channels/Select/main.go
@@ -1,33 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
-func main() {
-	c1 := make(chan int)
-	c2 := make(chan int)
+type Message struct {
+	id  int
+	Msg string
+}
 
+func main() {
+	c1 := make(chan Message)
+	c2 := make(chan Message)
+
+	// RabbitMQ
 	go func() {
-		time.Sleep(time.Second * 3)
-		c1 <- 1
+		time.Sleep(time.Second * 2)
+		msg := Message{1, "Hello from RABBIT mq"}
+		c1 <- msg
 	}()
 
+	// Kafka
 	go func() {
-		time.Sleep(time.Second * 4)
-		c2 <- 2
+		time.Sleep(time.Second * 1)
+		msg2 := Message{2, "Hello from KAFKA"}
+		c2 <- msg2
 	}()
 
 	// This select will choose the thread or channel faster
-	select {
-	case msg1 := <-c1:
-		println("received ", msg1)
+	for {
+		select {
+		case msg1 := <-c1: // rabbit mq
+			fmt.Printf("received FROM RABBITMQ %d :%s \n", msg1.id, msg1.Msg)
 
-	case msg2 := <-c2:
-		println("received ", msg2)
+		case msg2 := <-c2: // kafka
+			fmt.Printf("received FROM KAFKA %d :%s \n", msg2.id, msg2.Msg)
 
-	case <-time.After(time.Second * 3):
-		println("TIME OUT !!!")
+		case <-time.After(time.Second * 3):
+			println("TIME OUT !!!")
+		}
 	}
 
 }
