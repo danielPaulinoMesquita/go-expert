1959cc214ffd384788f458a9dfbdf6c76f311831
Daniel Paulino <daniel-paulino@hotmail.com>
2023-12-27 00:04:29 -0300

    93- Register Event.

---
+++ fourth-part-course/Events/pkg/events/events_dispatcher.go
@@ -0,0 +1,30 @@
+package events
+
+import "errors"
+
+var ErrHandlerAlreadyRegistered = errors.New("Handler already regisstered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return errors.New("Handler already registered")
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+
+}
