d32261fd968cd17b298bfbe062c00c28287b858a
Daniel Paulino <daniel-paulino@hotmail.com>
2024-01-07 23:46:53 -0300

    100- Creating a channel for Rabbit MQ and initialize consumer to consume messages from Rabbit

---
+++ fourth-part-course/Events/pkg/rabbitmq/rabbitmq.go
@@ -0,0 +1,43 @@
+package rabbitmq
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func OpenChannel() (*amqp.Channel, error) {
+	// this channel is different of channel from MultiTreading
+	conn, err := amqp.Dial("amqp://gues:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	return ch, nil
+}
+
+func Consume(channel *amqp.Channel, out chan<- amqp.Delivery) error {
+	msgs, err := channel.Consume(
+		"minhafila",
+		"go-consumer",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	for msg := range msgs {
+		out <- msg
+	}
+
+	return nil
+
+}
