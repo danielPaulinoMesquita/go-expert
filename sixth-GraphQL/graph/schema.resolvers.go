4af9a5a599052844d388bbff941563f3cc5244c3
Daniel Paulino <daniel-paulino@hotmail.com>
2024-01-18 02:11:25 -0300

    112- Created Resolver QueryCourse GraphQL

--- sixth-GraphQL/graph/schema.resolvers.go
+++ sixth-GraphQL/graph/schema.resolvers.go
@@ -6,8 +6,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
-
 	"github.com/dan/graphql/graph/model"
 )
 
@@ -61,7 +59,21 @@ func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, erro
 
 // Courses is the resolver for the courses field.
 func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
-	panic(fmt.Errorf("not implemented: Courses - courses"))
+	courses, err := r.CourseDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var courseModel []*model.Course
+	for _, course := range courses {
+		courseModel = append(courseModel, &model.Course{
+			ID:          course.ID,
+			Name:        course.Name,
+			Description: &course.Description,
+		})
+	}
+
+	return courseModel, nil
 }
 
 // Mutation returns MutationResolver implementation.
