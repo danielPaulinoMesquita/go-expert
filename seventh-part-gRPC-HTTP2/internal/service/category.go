9e2cf099182ad56b98c3e91d14ef3685634cc2f0
Daniel Paulino <daniel-paulino@hotmail.com>
2024-01-30 12:52:19 -0300

    121- Creating ListCategories gRPC

--- seventh-part-gRPC-HTTP2/internal/service/category.go
+++ seventh-part-gRPC-HTTP2/internal/service/category.go
@@ -17,7 +17,7 @@ func NewCategoryService(categoryDB database.Category) *CategoryService {
 	}
 }
 
-func (c *CategoryService) CreateCategory(ctx context.Context, request *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+func (c *CategoryService) CreateCategory(ctx context.Context, request *pb.CreateCategoryRequest) (*pb.Category, error) {
 	category, err := c.CategoryDB.Create(request.Name, request.Description)
 	if err != nil {
 		return nil, err
@@ -29,8 +29,25 @@ func (c *CategoryService) CreateCategory(ctx context.Context, request *pb.Create
 		Description: category.Description,
 	}
 
-	return &pb.CategoryResponse{
-		Category: categoryResponse,
-	}, nil
+	return categoryResponse, nil
+}
 
+func (c *CategoryService) ListCategories(ctx context.Context, request *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+
+	for _, category := range categories {
+		categoryResponse := &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		}
+
+		categoriesResponse = append(categoriesResponse, categoryResponse)
+	}
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
 }
