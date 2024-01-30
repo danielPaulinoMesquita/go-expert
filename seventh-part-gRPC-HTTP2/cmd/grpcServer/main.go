06e08b2fb11f1bc1c489eb414396497d9b3bee07
Daniel Paulino <daniel-paulino@hotmail.com>
2024-01-29 23:20:49 -0300

    119- Create a Server gRPC

---
+++ seventh-part-gRPC-HTTP2/cmd/grpcServer/main.go
@@ -0,0 +1,37 @@
+package grpcServer
+
+import (
+	"database/sql"
+	"github.com/daniel/grpc/internal/database"
+	"github.com/daniel/grpc/internal/pb"
+	"github.com/daniel/grpc/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
