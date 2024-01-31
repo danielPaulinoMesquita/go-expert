9b656cc386db22df424289d6bd44fd08536113f4
Daniel Paulino <daniel-paulino@hotmail.com>
2024-01-31 15:09:49 -0300

    125- GO CLI, Cobra Cli.

---
+++ eighth-part-course-CLI-Cobra/internal/database/course.go
@@ -0,0 +1,66 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+)
+
+type Course struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+	CategoryID  string
+}
+
+func NewCourse(db *sql.DB) *Course {
+	return &Course{db: db}
+}
+
+func (c *Course) Create(name, description, categoryID string) (*Course, error) {
+	id := uuid.New().String()
+	_, err := c.db.Exec("INSERT INTO courses (id, name, description, category_id) values ($1, $2, $3, $4)",
+		id, name, description, categoryID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Course{
+		ID:          id,
+		Name:        name,
+		Description: description,
+		CategoryID:  categoryID,
+	}, nil
+
+}
+
+func (c *Course) FindAll() ([]Course, error) {
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var courses []Course
+
+	for rows.Next() {
+		var id, name, description, categoryID string
+		if err := rows.Scan(&id, &name, &description, &categoryID); err != nil {
+			return nil, err
+		}
+		courses = append(courses, Course{ID: id, Name: name, Description: description, CategoryID: categoryID})
+	}
+	return courses, nil
+}
+
+func (c *Course) FindByCourseID(courseID string) (Category, error) {
+	var id, name, description string
+	err := c.db.QueryRow("SELECT c.id, c.name, c.description FROM categories c JOIN courses co ON c.id=co.category_id WHERE co.id = $1", courseID).Scan(&id, &name, &description)
+	if err != nil {
+		return Category{}, err
+	}
+
+	return Category{ID: id, Name: name, Description: description}, nil
+
+}
