c7e9b2a2153334dea40f59e9caa5fb170b45105c
Daniel Paulino <daniel-paulino@hotmail.com>
2024-02-07 01:28:15 -0300

    137- Creating CRUD with SQLC.

--- ninth-SQLC-DATABASE/sql/queries/query.sql
+++ ninth-SQLC-DATABASE/sql/queries/query.sql
@@ -1,5 +1,17 @@
 -- name: ListCategories :many
 SELECT * FROM categories;
 
+-- name: GetCategory :one
+SELECT * FROM categories
+where id = ?;
 
+-- name: CreateCategory :exec
+INSERT INTO categories (id, name, description)
+values (?,?,?);
 
+-- name: UpdateCategory :exec
+UPDATE categories SET name = ?, description = ?
+WHERE id = ?;
+
+-- name: DeleteCategory :exec
+DELETE FROM categories WHERE id = ?;
